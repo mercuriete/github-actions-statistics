49e4e6fecbe2f508de75fb165eb071fade968f18
Abel Garcia Dorta <abelgarcia@acidtango.com>
2021-05-10 15:03:53 +0100

    feat: list all public repos from an organization

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import { Octokit } from "@octokit/rest";
+import commandLineArgs, { OptionDefinition } from "command-line-args";
+
+const optionDefinitions: OptionDefinition[] = [
+  { name: "organization", alias: "o", type: String, defaultValue: "octokit" },
+];
+
+const options = commandLineArgs(optionDefinitions);
 
 const octokit = new Octokit();
 octokit.rest.repos
   .listForOrg({
-    org: "octokit",
+    org: options.organization,
     type: "public",
   })
   .then(({ data }) => {
